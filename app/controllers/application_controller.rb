e582cc9c61608e425cda3a2e299110c0e1a327fd
Haneum-Park <qkrgksdma17@gmail.com>
2020-03-26 18:53:56 +0900

    layout 변경

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def pageTitle
+        @title = "코로나, 함께해요!"
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def pageTitle
+    @title = "코로나, 함께해요!"
+  end
 end
