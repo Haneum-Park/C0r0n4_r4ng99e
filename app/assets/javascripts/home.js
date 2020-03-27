d160e24808d5330277a7f2ccc8e2844dfbb7a298
Haneum-Park <56185540+Haneum-Park@users.noreply.github.com>
2020-03-27 10:45:27 +0900

    layout 변경 (#24)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,52 +1,47 @@
-$('.dropdown-toggle').dropdown();
+$(document).ready(function () {
+    $('.dropdown-btn').dropdown();
 
-$(document).ready(function() {
-    $(".nav-link, .support_link").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+    var date = new Date();
+    var week = new Array('weekend', 'mon', 'tue', 'wed', 'thu', 'fri', 'weekend');
 
-            // Store hash
-            var hash = this.hash;
+    $("." + week[date.getDay()]).addClass("week");
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 500, function () {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+    $(".navbar-nav .nav-link, .dropdown-link").on('click', function (event) {
+        event.preventDefault();
+        var hrefTarget = $(this).attr("href");
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({ scrollTop: $(hrefTarget).offset().top }, 500);
     });
-    var sidebar_toggled_btn = document.getElementById('sidebarToggledBtn');
-    var sidebar_toggled = document.getElementById('sidebarToggled');
-    var content_toggled = document.getElementById('contentToggled');
-    
-    var sidebar_length = sidebar_toggled.classList.length;
-    var content_length = content_toggled.classList.length;
 
-    sidebar_toggled_btn.onclick = function() {
-        if ((sidebar_toggled.classList[sidebar_length - 1] == "toggled") && (content_toggled.classList[content_length - 1] == "toggled")) {
-            sidebar_toggled.classList.remove("toggled");
-            content_toggled.classList.remove("toggled");
+    window.onscroll = function () { navbarTop(); };
+
+    function navbarTop() {
+        var navbar = $("#navbar-top");
+
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            navbar.addClass("nav-top-fade-in");
+            navbar.removeClass("nav-top-fade-out");
         } else {
-            sidebar_toggled.classList.add("toggled");
-            content_toggled.classList.add("toggled");
+            navbar.removeClass("nav-top-fade-in");
+            navbar.addClass("nav-top-fade-out");
         }
-    };
-    window.onresize = function() {
-        if(window.outerWidth < 768) {
-            sidebar_toggled.classList.add("toggled");
-            content_toggled.classList.add("toggled");
-        } else {
-            sidebar_toggled.classList.remove("toggled");
-            content_toggled.classList.remove("toggled");
-        }
-        
     }
-    
-    window.onresize();
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+// });
+
+$.fn.scrollStopped = function (callback) {
+    var that = this, $this = $(that);
+    $this.scroll(function (ev) {
+        clearTimeout($this.data('scrollTimeout'));
+        $this.data('scrollTimeout', setTimeout(callback.bind(that), 200, ev));
+    });
+};
+
+$(window).scrollStopped(function (ev) {
+    $("#navbar-top").removeClass("nav-top-fade-in");
+    $("#navbar-top").addClass("nav-top-fade-out");
 });
