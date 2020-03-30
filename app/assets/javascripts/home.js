6a05fb49681514d3e5a7563aee2d0b0688eb0c1d
Haneum-Park <qkrgksdma17@gmail.com>
2020-03-30 13:17:34 +0900

    viewport 수정

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,46 +1,90 @@
-$(document).ready(function () {
-    $('.dropdown-btn').dropdown();
+document.addEventListener("DOMContentLoaded", function () {
+    // Bootstrap jQuery functions
+    $(".dropdown-btn").dropdown();
 
+    // Mask Pharmacy Date detect
     var date = new Date();
-    var week = new Array('weekend', 'mon', 'tue', 'wed', 'thu', 'fri', 'weekend');
+    var week = new Array("weekend", "mon", "tue", "wed", "thu", "fri", "weekend");
 
-    $("." + week[date.getDay()]).addClass("week");
+    var today = document.getElementsByClassName(week[date.getDay()]);
+    for (var i = 0; i < today.length; i++) {
+        today[i].classList.add("week");
+    }
 
-    $(".navbar-nav .nav-link, .dropdown-link").on('click', function (event) {
-        event.preventDefault();
-        var hrefTarget = $(this).attr("href");
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({ scrollTop: $(hrefTarget).offset().top }, 500);
-    });
+    // Navbar, Dropdown link click Event for scroll location
+    var link_location = document.querySelector(".navbar-nav");
+    var nav_links = link_location.querySelectorAll(".nav-link");
+    var dropdown_links = link_location.querySelectorAll(".dropdown-link");
 
-    window.onscroll = function () { navbarTop(); };
-
-    function navbarTop() {
-        var navbar = $("#navbar-top");
-
-        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-            $.fn.scrollStopped = function (callback) {
-                var that = this, $this = $(that);
-                $this.scroll(function (ev) {
-                    clearTimeout($this.data('scrollTimeout'));
-                    $this.data('scrollTimeout', setTimeout(callback.bind(that), 200, ev));
+    function scrollMove(links_array) {
+        for (var j = 0; j < links_array.length; j++) {
+            links_array[j].addEventListener("click", function (e) {
+                e.preventDefault();
+                var hrefTarget = this.getAttribute("href");
+                var this_offset_height = document.querySelector(hrefTarget).offsetTop;
+                window.scrollTo({
+                    top: this_offset_height,
+                    behavior: "smooth"
                 });
-            };
+            }, false);
+        }
+    }
 
-            $(window).scrollStopped(function (ev) {
-                navbar.removeClass("nav-top-fade-in");
-                navbar.addClass("nav-top-fade-out");
-            });
-            navbar.removeClass("nav-top-fade-out");
-            navbar.addClass("nav-top-fade-in");
-            navbar.addClass("fixed-top");
+    scrollMove(nav_links);
+    scrollMove(dropdown_links);
+
+    // Window size detect function
+    var scroll_height = 700;
+
+    function windowSize(window_size) {
+        if ((window_size) > 1200) {
+            scroll_height = 650;
+        }
+        else if ((window_size) > 960 && (window_size) <= 1200) {
+            scroll_height = 700;
+        }
+        else if ((window_size) > 768 && (window_size) <= 960) {
+            scroll_height = 800;
+        }
+        else if ((window_size) > 575 && (window_size) <= 768) {
+            scroll_height = 850;
+        }
+        else if ((window_size) <= 575) {
+            scroll_height = 1300;
+        }
+    }
+
+    windowSize(window.innerWidth || document.body.clientWidth);
+
+    window.onresize = function () {
+        windowSize(window.innerWidth || document.body.clientWidth);
+    };
+
+    // Navbar Scroll animation Event
+    function navbarTop() {
+        var navbar = document.querySelector("#navbar-top");
+
+        if (document.body.scrollTop > scroll_height || document.documentElement.scrollTop > scroll_height) {
+            var timer = null;
+            window.addEventListener('scroll', function () {
+                if (timer !== null) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(function () {
+                    navbar.classList.remove("nav-top-fade-in");
+                    navbar.classList.add("nav-top-fade-out");
+                }, 150);
+            }, false);
+            navbar.classList.remove("nav-top-fade-out");
+            navbar.classList.add("nav-top-fade-in");
+            navbar.classList.add("fixed-top");
             document.getElementsByClassName("navbar-top-display")[0].style.display = "block";
         } else {
-            navbar.removeClass("nav-top-fade-in");
-            navbar.removeClass("fixed-top");
-            navbar.addClass("nav-top-fade-out");
+            navbar.classList.remove("nav-top-fade-in");
+            navbar.classList.remove("fixed-top");
+            navbar.classList.add("nav-top-fade-out");
             document.getElementsByClassName("navbar-top-display")[0].style.display = "none";
         }
     }
+    window.onscroll = function () { navbarTop(); }
 });
