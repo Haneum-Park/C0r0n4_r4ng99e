937a4b690130933c7918e25cd6dc423ab4f548f8
Haneum-Park <56185540+Haneum-Park@users.noreply.github.com>
2020-03-31 16:34:55 +0900

    Merge conflict (#44)
    * layout 변경
    
    * layout change
    
    * 오타수정완료
    
    * viewport 수정
    
    * link change
    
    * data loaded error
    
    * corona data resolve
    
    * folder release delete

--- app/assets/javascripts/release/data_mohw.js
+++
@@ -1,7 +0,0 @@
-var dict_mohw = {};
-dict_mohw["total_increased"] = 78;
-dict_mohw["total_Confirmed"] = 9661;
-dict_mohw["total_deisolated"] = 5228;
-dict_mohw["total_dead"] = 158;
-dict_mohw["total_percentage"] = 18.63;
-
--- app/assets/javascripts/release/release_common.js
+++
@@ -1,3 +0,0 @@
-var common_info = {};
-common_info["Last_Update"] = "21시 3분";
-
