0a25f2308cc7cac839c709942f4a7ea1af867861
Haneum-Park <qkrgksdma17@gmail.com>
2020-03-31 17:00:39 +0900

    merge conflict

--- app/assets/javascripts/corona_data.js
+++ app/assets/javascripts/corona_data.js
@@ -1,26 +1,28 @@
-function numberCounter(target_frame, target_number) {
-    this.count = 0; this.diff = 0;
-    this.target_count = parseInt(target_number);
-    this.target_frame = document.getElementById(target_frame);
-    this.timer = null;
-    this.counter();
-};
-numberCounter.prototype.counter = function () {
-    var self = this;
-    this.diff = this.target_count - this.count;
-
-    if (this.diff > 0) {
-        self.count += Math.ceil(this.diff / 5);
+class numberCounter {
+    constructor(target_frame, target_number) {
+        this.count = 0;
+        this.diff = 0;
+        this.target_count = parseInt(target_number);
+        this.target_frame = document.getElementById(target_frame);
+        this.timer = null;
+        this.counter();
     }
-
-    this.target_frame.textContent = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-    if (this.count < this.target_count) {
-        this.timer = setTimeout(function () { self.counter(); }, 30);
-    } else {
-        clearTimeout(this.timer);
+    counter() {
+        var self = this;
+        this.diff = this.target_count - this.count;
+        if (this.diff > 0) {
+            self.count += Math.ceil(this.diff / 5);
+        }
+        this.target_frame.textContent = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        if (this.count < this.target_count) {
+            this.timer = setTimeout(function () { self.counter(); }, 30);
+        }
+        else {
+            clearTimeout(this.timer);
+        }
     }
-};
+}
+;
 
 function update_info() {
     var today_date = new Date();
