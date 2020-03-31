3a0aeaee85aa3267798fe33284f0325777fc6e2e
KuuWangE <shellcodesniper@icloud.com>
2020-03-31 17:50:26 +0900

    Realtime Data Migration

--- app/assets/javascripts/corona_data.js
+++ app/assets/javascripts/corona_data.js
@@ -1,50 +1,52 @@
-function numberCounter(target_frame, target_number) {
-    this.count = 0; this.diff = 0;
-    this.target_count = parseInt(target_number);
-    this.target_frame = document.getElementById(target_frame);
-    this.timer = null;
-    this.counter();
-};
-numberCounter.prototype.counter = function () {
-    var self = this;
-    this.diff = this.target_count - this.count;
-
-    if (this.diff > 0) {
-        self.count += Math.ceil(this.diff / 5);
-    }
-
-    this.target_frame.textContent = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-
-    if (this.count < this.target_count) {
-        this.timer = setTimeout(function () { self.counter(); }, 30);
-    } else {
-        clearTimeout(this.timer);
-    }
+class numberCounter {
+	constructor(target_frame, target_number) {
+		this.count = 0;
+		this.diff = 0;
+		this.target_count = parseInt(target_number);
+		this.target_frame = document.getElementById(target_frame);
+		this.timer = null;
+		this.counter();
+	}
+	counter() {
+		var self = this;
+		this.diff = this.target_count - this.count;
+		if (this.diff > 0) {
+			self.count += Math.ceil(this.diff / 5);
+		}
+		this.target_frame.textContent = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		if (this.count < this.target_count) {
+			this.timer = setTimeout(function () {
+				self.counter();
+			}, 30);
+		} else {
+			clearTimeout(this.timer);
+		}
+	}
 };
 
 function update_info() {
-    var today_date = new Date();
-    this_month = (today_date.getMonth() + 1);
-    this_day = today_date.getDate();
+	var today_date = new Date();
+	this_month = (today_date.getMonth() + 1);
+	this_day = today_date.getDate();
 
-    var last_update = document.querySelectorAll(".last_update");
-    for (var update_count = 0; update_count < last_update.length; update_count++) {
-        last_update[update_count].textContent = this_month + "/" + this_day + " " + common_info["Last_Update"];
-    }
+	var last_update = document.querySelectorAll(".last_update");
+	for (var update_count = 0; update_count < last_update.length; update_count++) {
+		last_update[update_count].textContent = this_month + "/" + this_day + " " + common_info["Last_Update"];
+	}
 }
 
 function crawlered_data() {
-    new numberCounter("gwangju_confirmed", dict_gwangju['Confirmed']);
-    new numberCounter("gwangju_isolation", dict_gwangju['Hospital_Isolation'] + dict_gwangju['Self_Isolation'] + dict_gwangju['Facility_Isolation']);
-    new numberCounter("gwangju_recovered", dict_gwangju['Finish_Self_Isolation']);
-    new numberCounter("korea_confirmed", dict_mohw["total_Confirmed"]);
-    new numberCounter("korea_deisolated", dict_mohw["total_Confirmed"] - dict_mohw["total_deisolated"]);
-    new numberCounter("korea_isolated", dict_mohw["total_deisolated"]);
-    new numberCounter("korea_deaths", dict_mohw["total_dead"]);
+	new numberCounter("gwangju_confirmed", dict_gwangju['Confirmed']);
+	new numberCounter("gwangju_isolation", dict_gwangju['Hospital_Isolation'] + dict_gwangju['Self_Isolation'] + dict_gwangju['Facility_Isolation']);
+	new numberCounter("gwangju_recovered", dict_gwangju['Finish_Self_Isolation']);
+	new numberCounter("korea_confirmed", dict_mohw["total_Confirmed"]);
+	new numberCounter("korea_deisolated", dict_mohw["total_Confirmed"] - dict_mohw["total_deisolated"]);
+	new numberCounter("korea_isolated", dict_mohw["total_deisolated"]);
+	new numberCounter("korea_deaths", dict_mohw["total_dead"]);
 };
 
 
 window.addEventListener("DOMContentLoaded", function () {
-    update_info()
-    crawlered_data();
+	update_info()
+	crawlered_data();
 });
--- config/initializers/load_data.rb
+++ config/initializers/load_data.rb
@@ -1,6 +1,16 @@
 # ! 필요한 파일들을 로딩합니다.
 require 'json'
 
-file = File.read(Rails.root.join('app','assets','config','release_data','release_common.json'))
+Rails.application.configure do
+	file = File.read(Rails.root.join('app','assets','config','release_data','release_common.json'))
+	config.x.release_common = JSON.parse(file)
+	puts config.x.release_common
 
-Rails.application.config.x.release_common = JSON.parse(file)
+	file = File.read(Rails.root.join('app','assets', 'config', 'release_data', 'data_gwangju.json'))
+	config.x.data_gwangju = JSON.parse(file)
+	puts config.x.data_gwangju
+
+	file = File.read(Rails.root.join('app','assets', 'config', 'release_data', 'data_mohw.json'))
+	config.x.data_mohw = JSON.parse(file)
+	puts config.x.data_mohw
+end
